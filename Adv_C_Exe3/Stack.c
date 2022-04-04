f700c5421c700fd3cda4ab8b32b5ba8e5c21e96f
dangalg <dangalg@gmail.com>
2022-04-04 10:20:22 +0300

    added destroy pop and flipbetween
    Coded the functions:
    destroy
    pop
    flip between hashes

--- Adv_C_Exe3/Stack.c
+++ Adv_C_Exe3/Stack.c
@@ -1,6 +1,9 @@
 #include "Stack.h"
-
+#include <stdio.h>
 /***************** Stack ADT Implementation *****************/
+
+void removeHead(charNode** head);
+void printStack(const Stack* s);
 
 void initStack(Stack* s)
 {
@@ -9,7 +12,17 @@ void initStack(Stack* s)
 
 void destroyStack(Stack* s)
 {
-	// add your code here
+	// get first object
+	charNode* tmp = s->head;
+	charNode* next = NULL;
+	while (tmp != NULL)
+	{
+		next = tmp->next;
+		free(tmp);
+		tmp = next;
+	}
+
+	s->head = NULL;
 }
 
 void push(Stack* s, char data)
@@ -20,8 +33,12 @@ void push(Stack* s, char data)
 char pop(Stack* s)
 {
 	// add your code here
+	char res;
+	if (s != NULL) { printf("\nStack is empty"); return NULL; }
+	res = s->head->data;
+	removeHead(s->head);
+	return res;
 }
-
 
 int isEmptyStack(const Stack* s)
 {
@@ -32,7 +49,53 @@ int isEmptyStack(const Stack* s)
 
 void flipBetweenHashes(const char* sentence)
 {
+	if (sentence == NULL)
+	{
+		return;
+	}
+
 	// add your code here
+	Stack* tmpStack = NULL;
+	initStack(tmpStack);
+
+	// counts the amount of hashes
+	int hashAmount = 0;
+
+	while (*sentence)
+	{
+		// catch # and decide what to do
+		char tmpChar = *sentence;
+		if (tmpChar == "#")
+		{
+			// increment hash amount
+			hashAmount++;
+
+			// after the second has print the temp stack and empty it
+			if (hashAmount == 2)
+			{
+				// reset has amount
+				hashAmount = 0;
+
+				// print and destroy temp stack
+				printStack(tmpStack);
+				destroyStack(tmpStack);
+			}
+		}
+		
+		// no hashes just print the char
+		if (hashAmount == 0)
+		{
+			printf("%c", tmpChar);
+		}
+
+		// between hashes insert chars to temp stack
+		if (hashAmount == 1)
+		{
+			push(tmpStack, tmpChar);
+		}
+
+		sentence++;
+	}
 }
 
 int isPalindrome(Stack* s)
@@ -43,4 +106,26 @@ int isPalindrome(Stack* s)
 void rotateStack(Stack* s, int n)
 {
 	// add your code here
+}
+
+void removeHead(charNode** head)
+{
+	if (head == NULL)
+	{
+		return;
+	}
+
+	charNode* tmp = (*head)->next;
+	free(*head);
+	(*head) = tmp;
+}
+
+void printStack(const Stack* s)
+{
+	charNode* tmp = s->head;
+	while (tmp != NULL)
+	{
+		printf("%c", tmp->data);
+		tmp = tmp->next;
+	}
 }
